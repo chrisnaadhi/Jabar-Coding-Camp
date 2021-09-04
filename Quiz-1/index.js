9a9c4712198713f858880a08d2fd438b7e2c62d2
chrisnaadhi <chrisnaadhip@gmail.com>
2021-09-04 17:16:48 +0700

    Soal Nomor 1

---
+++ Quiz-1/index.js
@@ -0,0 +1,11 @@
+// SOAL 1
+
+function jumlah_kata(kata) {
+    console.log(kata.split(' ').length);
+}
+
+var kalimat_1 = "Halo Nama Saya Muhammad Iqbal Mubarok"
+var kalimat_2 = "Saya Iqbal"
+
+jumlah_kata(kalimat_1);
+jumlah_kata(kalimat_2);
